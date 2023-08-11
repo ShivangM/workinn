f62ffc19349790f3eaf388475432ec9fdd400a95
ShivangM <shivangmishra0824@gmail.com>
2023-08-12 00:45:31 +0530

    Navbar Created

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const page = (props: Props) => {
+  return <div>about</div>;
+};
+
+export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        brand: '#2EC4B6',
+        primary: '#FF9F1C',
+        secondary: '#FFBF69',
+        tertiary: '#CBF3F0',
+        light: '#FFFFFF',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
