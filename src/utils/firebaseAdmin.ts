5acd0cc56ccbb42f833767c62de4787cf016eaf9
ShivangM <shivangmishra0824@gmail.com>
2023-10-20 20:33:49 +0530

    Firebase Admin Env Fix

--- src/utils/firebaseAdmin.ts
+++ src/utils/firebaseAdmin.ts
@@ -1,6 +1,9 @@
 import { ServiceAccount } from 'firebase-admin/app';
 import admin from 'firebase-admin';
-import serviceAccount from '../../serviceAccountKey.json';
+
+const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+);
 
 try {
     admin.initializeApp({
