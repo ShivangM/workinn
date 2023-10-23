5480cb0337dfb507d20902becc7445741c9bd9c3
ShivangM <shivangmishra0824@gmail.com>
2023-10-23 23:43:26 +0530

    Invalid URL Fix

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,5 +1,5 @@
 const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : process.env.VERCEL_URL;
+    : `https://${process.env.VERCEL_URL}`;
 export default BASE_URL;
