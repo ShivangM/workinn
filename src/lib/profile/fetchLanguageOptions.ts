0e183e9025f26e2e9570fc165801cfa8aad56a38
ShivangM <shivangmishra0824@gmail.com>
2023-10-25 10:48:26 +0530

    Cors Fix

--- src/lib/profile/fetchLanguageOptions.ts
+++ src/lib/profile/fetchLanguageOptions.ts
@@ -4,7 +4,12 @@ import BASE_URL from '@/utils/baseUrl';
 const fetchLanguageOptions = async (
     searchTerm?: string,
 ): Promise<Language[]> => {
-    const res = await fetch(searchTerm ? `${BASE_URL}/api/languages?searchTerm=${searchTerm}` : `${BASE_URL}/api/languages`).then((res) => res.json());
+    const res = await fetch(searchTerm ? `${BASE_URL}/api/languages?searchTerm=${searchTerm}` : `${BASE_URL}/api/languages`, {
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        mode: "no-cors"
+    }).then((res) => res.json());
     return res.languages;
 };
 
