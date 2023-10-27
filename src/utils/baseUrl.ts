fe6fcc6c157fd6dce43ccd055855f5bb48aa3879
ShivangM <shivangmishra0824@gmail.com>
2023-10-27 23:18:32 +0530

    Added Public URL for origin error fix

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,5 +1,5 @@
 const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    : process.env.NEXT_PUBLIC_URL;
 export default BASE_URL;
