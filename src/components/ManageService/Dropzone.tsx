de27b0e6a4dcf786f49a1d56bf13bae8034d965a
Pranay-Thakre <thakrepranay62@gmail.com>
2023-10-27 22:09:16 +0530

    Lint Fix

--- src/components/ManageService/Dropzone.tsx
+++ src/components/ManageService/Dropzone.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import { FileWithPath, useDropzone } from 'react-dropzone';
 import { Control, useController } from 'react-hook-form';
@@ -33,15 +34,16 @@ function Dropzone({ control, name }: DropzoneProps) {
 
     const thumbs = files.map((file) => (
         <div className="inline-flex border border-gray-300 rounded m-1 p-1" key={file.name}>
-            <div className="flex h-20 aspect-auto flex-1 min-w-0 overflow-hidden">
-                <img
+            <div className="flex h-20 relative aspect-auto flex-1 min-w-0 overflow-hidden">
+                <Image
                     src={file.preview}
                     className="block w-auto h-full"
+                    fill
                     // Revoke data URI after the image is loaded
                     onLoad={() => {
                         if (file.preview) URL.revokeObjectURL(file.preview);
                     }}
-                    alt="uploaded"
+                    alt={file.name}
                 />
             </div>
         </div>
@@ -58,7 +60,7 @@ function Dropzone({ control, name }: DropzoneProps) {
         <div className="container mx-auto p-4">
             <div {...getRootProps({ className: 'dropzone border-dashed border-2 border-gray-300 p-4' })}>
                 <input {...getInputProps()} />
-                <p className="text-center">Drag 'n' drop some files here (Max 5), or click to select files</p>
+                <p className="text-center">Drag &apos;n&apos; drop some files here (Max 5), or click to select files</p>
             </div>
             <div className="flex flex-wrap mt-4">{thumbs}</div>
         </div>
